4b537378568f2144676c4113ec50f1435eb80587
杉山浩平 <kou@MacBook-Pro.local>
2019-07-21 15:21:00 +0900

    課題に取り組みました

---
+++ main.js
@@ -0,0 +1,23 @@
+const lastName = "山田";
+const firstName = "太郎";
+const fullName = lastName + " " + firstName;
+const myAge = 10;
+const friendAge = 13;
+const totalAge = myAge + friendAge;
+const numberOfDays = totalAge * 23;
+const numberOfChildren = 3;
+const numberOfCakes = 10;
+const nmberOfCakesByChildu = numberOfCakes / numberOfChildren;
+const numberOfRestOfCakes = numberOfChildren % numberOfCakes;
+
+console.log(lastName);
+console.log(firstName);
+console.log(fullName);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOfDays);
+console.log(numberOfChildren);
+console.log(numberOfCakes);
+console.log(nmberOfCakesByChildu);
+console.log(numberOfRestOfCakes);
